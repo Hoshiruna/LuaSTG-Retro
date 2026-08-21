ebb511dfcbfb747807fc460c8b7a5a999f5cc962
Hoshiruna <hoshiruna05@gmail.com>
2026-08-21 08:52:27 +0800

    Portable build and module boundaries

--- engine/graphics/d3d11/DisplayModeHelper.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "d3d11/pch.h"
-
-namespace d3d11
-{
-    bool getSwapChainNearestOutputFromDisplay(IDXGISwapChain* swap_chain, HMONITOR display, IDXGIOutput** out_output);
-
-    bool findBestDisplayMode(IDXGISwapChain1* swap_chain, HMONITOR display, UINT target_width, UINT target_height, DXGI_MODE_DESC1& mode);
-}
--- engine/graphics/d3d11/FormatHelper.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "core/GraphicsFormat.hpp"
-#include "d3d11/pch.h"
-
-namespace d3d11
-{
-    DXGI_FORMAT toFormat(core::GraphicsFormat format);
-}
